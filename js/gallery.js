c5e5471513cc055fd3b64cc9a7bb9b7534c099a2
DzyadzoMM <m.dziadzo@gmail.com>
2025-05-25 03:50:48 +0300

    update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -38,12 +38,12 @@ const images = [
   preview:'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
   original:'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
   description: 'Nature Landscape',
-},/*
+},
 {
   preview:'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
   original:'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
   description: 'Lighthouse Coast Sea',
-},*/
+},
 ];
 const container = document.querySelector(".gallery");
 
